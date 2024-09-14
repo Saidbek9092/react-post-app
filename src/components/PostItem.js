32b81dfbb68670657c40624971205a6af1d451c1
saidbek2023 <saidbek9092@gmail.com>
2024-09-15 00:35:56 +0500

    BRANCH B

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,13 +1,12 @@
 import classes from "./PostItem.module.css";
 
-
 // Just featB first commit
 // another commit
 
 function PostItem({ post }) {
   return (
     <article className={classes.item}>
-      <h1>{post.title}</h1>
+      <h1>{post.title} branchB</h1>
       <p>{post.body}</p>
     </article>
   );
